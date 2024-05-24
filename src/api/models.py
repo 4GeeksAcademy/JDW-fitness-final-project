34c79011f3b2de2cf75ee9daf2c98e572c94d87b
Walter Rivero <127254776+walter10x@users.noreply.github.com>
2024-05-24 08:12:51 +0000

    create model diseases

--- src/api/models.py
+++ src/api/models.py
@@ -17,3 +17,20 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Diseases(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    kind = db.Column(db.String(120), unique=True, nullable=False)
+    sintoms = db.Column(db.String(80), unique=False, nullable=False)
+    
+
+    def __repr__(self):
+        return f'<Diseases {self.kind}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.kind,
+            "sintoms": self.sintoms
+            # do not serialize the password, its a security breach
+        }
